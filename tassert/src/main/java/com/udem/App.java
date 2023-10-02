b281d65ececc1d5a78fb716d6c097243f7e14e76
Hichem Agrane <hichemagrane@gmail.com>
2023-10-02 10:23:29 -0400

    initiate project and complete tassert

---
+++ tassert/src/main/java/com/udem/App.java
@@ -0,0 +1,36 @@
+package com.udem;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class App 
+{
+    public static void main( String[] args ) {
+        
+        
+        String filePath = args[0];
+
+        try (FileReader fileReader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            String line;
+            int result = 0;
+
+            // Read the file line by line
+            while ((line = bufferedReader.readLine()) != null) {
+                line = line.trim();
+                if (line.startsWith("assert") || line.startsWith("fail")) {
+                    result++;
+                }
+            }
+
+            System.out.println(result);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       
+    }
+}
