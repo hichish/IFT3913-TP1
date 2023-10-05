4e640e2cd6860491af2e539faa54bffaf0ebf81d
duomo <liduomo1997@gmail.com>
2023-10-05 19:13:02 -0400

    version le bug corrige

--- tloc/TLOC.java
+++ tloc/TLOC.java
@@ -2,10 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class TLOC {
+public class TLOC{
     public static void main(String[] args) {
-        String filePath = "test.java"; // Remplacez "VotreClasse.java" par le chemin de votre fichier source
-        
+        if (args.length != 1) {
+            System.out.println("Utilisation : java TLOC <chemin_vers_fichier>");
+            System.exit(1);
+        }
+        String filePath = args[0];
         try {
             int linesOfCode = countLinesOfCode(filePath);
             System.out.println(linesOfCode);
@@ -17,37 +20,32 @@ public class TLOC {
     public static int countLinesOfCode(String filePath) throws IOException {
         int count = 0;
         boolean inCommentBlock = false;
-
+    
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
-
+    
             while ((line = reader.readLine()) != null) {
                 line = line.trim();
-
+    
                 if (line.isEmpty()) {
                     continue; // Ignorer les lignes vides
                 }
-
+    
                 if (line.startsWith("/*")) {
                     inCommentBlock = true;
-                    continue;
                 }
-
-                if (inCommentBlock) {
-                    if (line.contains("*/")) {
-                        inCommentBlock = false;
-                        line = line.substring(line.indexOf("*/") + 2).trim();
-                    } else {
-                        continue; // Ignorer les lignes à l'intérieur des blocs de commentaires
-                    }
-                }
-
+    
                 if (!inCommentBlock && !line.startsWith("//")) {
                     count++;
                 }
+    
+                if (line.contains("*/")) {
+                    inCommentBlock = false;
+                    line = line.substring(line.indexOf("*/") + 2).trim();
+                }
             }
         }
-
+    
         return count;
     }
 }
